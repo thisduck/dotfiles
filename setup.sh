affed643079b522a67212fe97e8c9ee6dca5eee5
Adnan Ali <adnan.ali@gmail.com>
2022-01-31 19:43:21 -0500

    Remove vimscript base configs.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 rm -rf ~/.local/share/nvim/site/pack/packer/start/packer.nvim && git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
